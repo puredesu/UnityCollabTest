58e81a03d69982c907691b57bec1ae4649e6c834
Puredesu <realpuredesu@gmail.com>
2022-10-28 10:15:55 -0400

    Added player movement

--- UnityCollabTestGame/Assets/Scripts/Player.cs
+++ UnityCollabTestGame/Assets/Scripts/Player.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    [SerializeField] private float playerSpeed = 5;
+
+    private CharacterController controller;
+    private Vector3 move;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // x,z movement
+        move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(playerSpeed * Time.deltaTime * move);
     }
 }
