37d6d1025b54625772228d9da5693fccd15e2ecd
Puredesu <realpuredesu@gmail.com>
2022-10-29 14:56:43 -0400

    Added shooting mechanics

---
+++ UnityCollabTestGame/Assets/Scripts/Bullet.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private float speed = 50f;
+    [SerializeField] private float lifeTime = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.forward * speed;
+        Destroy(gameObject, lifeTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ UnityCollabTestGame/Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private Transform firePoint;
+    [SerializeField] private float cooldown = 0.3f;
+    private bool isBulletReady = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        if (!isBulletReady)
+        {
+            return;
+        }
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        StartCoroutine(StartCooldown());
+    }
+
+    private IEnumerator StartCooldown()
+    {
+        isBulletReady = false;
+        yield return new WaitForSeconds(cooldown);
+        isBulletReady = true;
+    }
+}
