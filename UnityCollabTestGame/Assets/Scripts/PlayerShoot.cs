917aa6348046b9bef07b9e8683160540d18a3ec1
Puredesu <realpuredesu@gmail.com>
2022-10-31 06:52:02 -0400

    Player shoot key changed from Space to Left Click

--- UnityCollabTestGame/Assets/Scripts/PlayerShoot.cs
+++ UnityCollabTestGame/Assets/Scripts/PlayerShoot.cs
@@ -7,13 +7,16 @@ public class PlayerShoot : MonoBehaviour
 
     [SerializeField] private GameObject bulletPrefab;
     [SerializeField] private Transform firePoint;
+
+    // Default values
     [SerializeField] private float cooldown = 0.2f;
+
     private bool isBulletReady = true;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Mouse0))
         {
             Shoot();
         }
